b1da270245a7e23e18029e1754244eb36796e424
chirag9899 <chirag0264@gmail.com>
2023-08-25 10:14:04 +0000

    validation

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,21 +3,21 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer class="bg-white dark:bg-gray-900">
-            <div class="container px-6 py-6 mx-auto">
+        <footer className="bg-white dark:bg-gray-900">
+            <div className="container px-6 py-6 mx-auto">
                
 
-                <hr class="my-4 border-gray-200 dark:border-gray-700" />
+                <hr className="my-4 border-gray-200 dark:border-gray-700" />
 
-                <div class="flex flex-col items-center sm:flex-row sm:justify-between">
-                    <p class="text-sm text-gray-500">© Copyright 2021. All Rights Reserved.</p>
+                <div className="flex flex-col items-center sm:flex-row sm:justify-between">
+                    <p className="text-sm text-gray-500">© Copyright 2021. All Rights Reserved.</p>
 
-                    <div class="flex mt-3 -mx-2 sm:mt-0">
-                        <Link href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Teams </Link>
+                    <div className="flex mt-3 -mx-2 sm:mt-0">
+                        <Link href="#" className="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Teams </Link>
 
-                        <Link href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Privacy </Link>
+                        <Link href="#" className="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Privacy </Link>
 
-                        <Link href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Cookies </Link>
+                        <Link href="#" className="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Cookies </Link>
                     </div>
                 </div>
             </div>
--- app/components/welcomepage/Connectwallet.jsx
+++ app/components/welcomepage/Connectwallet.jsx
@@ -1,46 +1,56 @@
-import React from 'react'
+import React from 'react';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import { connectWallet, disconnect } from "@/app/redux/feature/connect-wallet-slice"
+import { connectWallet, disconnect } from "@/app/redux/feature/connect-wallet-slice";
 import { useDispatch, useSelector } from 'react-redux';
 import Link from 'next/link';
 
 const Connectwallet = () => {
   const dispatch = useDispatch();
-
   const userData = useSelector(state => state.connectWalletReducer);
   console.log(userData);
 
   return (
     <div className="bg-white w-[40vh] flex flex-col items-center p-2 gap-4 border rounded-lg ">
       <div className="px-6 py-4 border-b rounded-t">
-        <h3 className="text-base font-semibold text-gray-900 ">
-          Connect wallet
-        </h3>
-        {/* <button onClick={() => dispatch(disconnect())}>diconnect</button> */}
+        {userData.user.signer ? (
+          <h3 className="text-base font-semibold text-gray-900">
+            Disconnect wallet
+          </h3>
+        ) : (
+          <h3 className="text-base font-semibold text-gray-900">
+            Connect wallet
+          </h3>
+        )}
       </div>
 
       <div className="p-6">
         <p className="text-sm font-normal text-gray-500 dark:text-gray-400">Connect with one of our available wallet providers or create a new one.</p>
         <ul className="my-4 space-y-3">
           <li>
-            <div onClick={() => { console.log("hello world"); dispatch(connectWallet())}} className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow border">
+            <div onClick={() => { 
+              if (userData.user.signer) {
+                dispatch(disconnect());
+              } else {
+                console.log("hello world");
+                dispatch(connectWallet());
+              }
+            }} className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow border">
               <img src="https://assets-global.website-files.com/636e894daa9e99940a604aef/63bb99fc3d3d7a0f906e49ed_Keplr-logo.png" className='w-20' />
-              {/* <span className="flex-1 ml-3 whitespace-nowrap">Kepl</span> */}
-              <span className="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded ">Popular</span>
+              <span className={`inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 ${userData.user.signer ? 'bg-red-200' : 'bg-green-200'} rounded`}>
+                {userData.user.signer ? 'Disconnect' : 'Connect'}
+              </span>
             </div>
           </li>
         </ul>
         <div>
-          <Link href="https://www.keplr.app/download" 
-            className="inline-flex items-center text-xs font-normal text-gray-500 hover:underline ">
+          <Link href="https://www.keplr.app/download" className="inline-flex items-center text-xs font-normal text-gray-500 hover:underline">
             <HelpOutlineIcon className='w-3 h-3 mr-2' />
-            How to install Wallet? </Link>
+            How to install Wallet?
+          </Link>
         </div>
       </div>
-      
     </div>
-    
-  )
+  );
 }
 
-export default Connectwallet
+export default Connectwallet;
